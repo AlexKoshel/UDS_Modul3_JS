a5b4e604ecfff89e482245ccc7b39563ca00fe53
Alex Koshel <kovsh.07@gmail.com>
2021-05-26 19:29:28 +0300

    Add files via upload

---
+++ d365.js
@@ -0,0 +1,33 @@
+var Sdk = window.Sdk || {};
+(
+    function () {      
+
+        this.reservedPickupOnChange = function (executionContext) {
+            var formContext = executionContext.getFormContext();           
+            var reservedPickupDate = formContext.getAttribute("cr9d3_reserved_pickup").getValue();
+            var currentDay = new Date();
+
+            var reservedPickupDateFeildControl = formContext.getControl("cr9d3_reserved_pickup");
+            if (reservedPickupDate < currentDay) {
+                reservedPickupDateFeildControl.setNotification(" Please enter the correct date");
+            }
+            else {
+                reservedPickupDateFeildControl.clearNotification();
+            }
+        }
+
+        this.statusCodeOnChange = function (executionContext) {
+            var formContext = executionContext.getFormContext();
+            var statusCode = formContext.getAttribute("statuscode").getValue();
+            var isPaid = formContext.getAttribute("cr9d3_paid").getValue();
+            var statusCodeFieldControl = formContext.getControl("statuscode");
+
+            if ((statusCode == 970300001) && (!isPaid)) {
+                statusCodeFieldControl.setNotification("Car rent is not yet paid. Car cannot be rented");
+            }
+            else {
+                statusCodeFieldControl.clearNotification();
+            }
+        }
+    }
+).call(Sdk);
